c96dfc1f4629493f2a0cfa4e81ab0f4b40afdb7f
dvoraka <alen.dvorak@gmail.com>
2019-12-18 15:19:12 +0100

    #3 Move to Spring Boot 1

--- src/main/java/dvoraka/restcommmspoc/TransferApp.java
+++ src/main/java/dvoraka/restcommmspoc/TransferApp.java
@@ -1,13 +1,22 @@
 package dvoraka.restcommmspoc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 public class TransferApp {
     public static void main(String[] args) {
         SpringApplication.run(TransferApp.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            System.out.println("App");
+        };
     }
 }
