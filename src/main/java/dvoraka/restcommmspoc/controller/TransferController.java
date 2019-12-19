cde1f1d36de2240e24ca6adfe5f7e2fee3410e23
Alen Dvorak <alen.dvorak@gmail.com>
2019-12-19 23:29:13 +0100

    #3 Tuning

--- src/main/java/dvoraka/restcommmspoc/controller/TransferController.java
+++ src/main/java/dvoraka/restcommmspoc/controller/TransferController.java
@@ -1,16 +1,28 @@
 package dvoraka.restcommmspoc.controller;
 
 import dvoraka.restcommmspoc.data.message.TransferMessage;
+import dvoraka.restcommmspoc.service.RestServerTransferService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
 
+import static java.util.Objects.requireNonNull;
+
 @Slf4j
 @RestController
 public class TransferController {
+
+    private final RestServerTransferService service;
+
+
+    @Autowired
+    public TransferController(RestServerTransferService service) {
+        this.service = requireNonNull(service);
+    }
 
     @PostMapping(ControllerConstants.SAVE_PATH)
     public void save(@RequestBody TransferMessage message) {
